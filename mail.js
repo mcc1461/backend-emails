1d207575937c0de27a6bb8bd1518b666b4d70396
mcc1461 <mcoskuncelebi@gmail.com>
2024-07-10 05:53:12 +0300

    fix: Gmail error (Error: Invalid login: 535-5.7.8 Username and Password not accepted) fixed.

--- mail.js
+++ mail.js
@@ -1,50 +1,49 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const path = require("path");
 
 const transporter = nodemailer.createTransport({
-    service: process.env.MAIL_SERVICE,
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: false,
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-    }   
+  service: process.env.MAIL_SERVICE,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
 });
 
 const mailOptions = {
-        from: {
-         name: "MusCo Blog",
-            address: process.env.MAIL_USER
-        },    
-        to: ["musco@gmail.com", "mcoskuncelebi@gmail.com"],
-        subject: "Welcome to MusCo Blog App - Testing Nodemailer for sending emails",
-        text: "Hello, welcome to MusCo Blog App. We are glad to have you here. We hope you enjoy our content. Feel free to reach out to us if you have any questions.",
-        html: `<b>Hello User...</b>`,
-        attachments: [
-            {
-                filename: "musco.png",
-                path: path.json(__dirname, "musco.png"),
-                contentType: "image/png"
-            },
-            {
-                filename: "musco.pdf",
-                path: path.json(__dirname, "musco.pdf"),
-                contentType: "application/pdf"
-            }
-        ]
-    };
+  from: {
+    name: "MusCo Blog",
+    address: process.env.MAIL_USER,
+  },
+  to: ["mcoskuncelebi@gmail.com"],
+  subject: "Welcome to MusCo Blog App - Testing Nodemailer for sending emails",
+  text: "Hello, welcome to MusCo Blog App. We are glad to have you here. We hope you enjoy our content. Feel free to reach out to us if you have any questions.",
+  html: `<b>Hello User...</b>`,
+  attachments: [
+    {
+      filename: "MusCo.png",
+      path: path.join(__dirname, "MusCo.png"),
+      contentType: "image/png",
+    },
+    {
+      filename: "MusCo.pdf",
+      path: path.join(__dirname, "MusCo.pdf"),
+      contentType: "application/pdf",
+    },
+  ],
+};
 
-const sendEmail = async (mailOptions) => {
-    try {
-        const info = await transporter.sendMail(mailOptions);
-        console.log("Message sent: %s", info.messageId);
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    } catch (error) {
-        console.error("Error occurred: ", error);
-    }
-}
+const sendEmail = async () => {
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.error("Error occurred: ", error);
+  }
+};
 
-
-module.exports = {sendEmail};
-
-
+module.exports = { sendEmail };
--- server.js
+++ server.js
@@ -1,53 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { sendEmail } = require("./mail");
 
 const app = express();
 const port = process.env.PORT || 3007;
 
-
-//parse application/x-www-form-urlencoded
+// Parse application/json
+app.use(express.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//parse application/json
-app.use(bodyParser.json());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Welcome to MusCo Blog");
+app.get("/email-send", async (req, res) => {
+  try {
+    await sendEmail();
+    res.status(200).send("Email sent successfully!");
+  } catch (error) {
+    res.status(500).send("Error occurred: " + error.message);
+  }
 });
 
-
-const nodemailer = require("nodemailer");
-
-app.post("/email-send", async (req, res) => {
-    const { name, email } = req.body;
-    //create reusable transporter object using the default SMTP transport
-const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // Use `true` for port 465, `false` for all other ports
-  auth: {
-    user: "river.dicki12@ethereal.email",
-    pass: "Ds1XV3rSHysTP68HRd",
-  },
-});
-
-const msg = {
-    from: `"MusCo Blog" <musco.blog@bloggers.com>`, // sender address
-    to: `${name} <${email}>`, // list of receivers
-    subject: "Welcome to the Express App", // Subject line
-    text: `Hello ${name}, welcome to the Express App`, // plain text body
-};
-// send mail with defined transport object
-const info = await transporter.sendMail(msg);
-res.send(`Message have been sent successfully via NODEMAILER! 
-Preview URL: ${nodemailer.getTestMessageUrl(info)}
-${name} <${email}>
-${info.response}`);
-
-});
-
-
-app.listen(3007, () => {
+app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
