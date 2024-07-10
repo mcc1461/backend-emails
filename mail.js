cbab6ff0e4eb2f80012f63ed109e26d12d245be7
mcc1461 <mcoskuncelebi@gmail.com>
2024-07-10 07:24:04 +0300

    .

--- mail.js
+++ mail.js
@@ -13,6 +13,10 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+// Define the base message content
+const messageContent =
+  "Hello, welcome to MusCo Blog App. We are glad to have you here. We hope you enjoy our content. Feel free to reach out to us if you have any questions.";
+
 const mailOptions = {
   from: {
     name: "MusCo Blog",
@@ -20,8 +24,14 @@ const mailOptions = {
   },
   to: ["mcoskuncelebi@gmail.com"],
   subject: "Welcome to MusCo Blog App - Testing Nodemailer for sending emails",
-  text: "Hello, welcome to MusCo Blog App. We are glad to have you here. We hope you enjoy our content. Feel free to reach out to us if you have any questions.",
-  html: `<b>Hello User...</b>`,
+  text: messageContent, // Use the message content in the text field
+  html: `<b>Hello User,</b>
+    <br><br>
+    ${messageContent.replace(/\n/g,"<br>")}
+    <br><br>
+    Best regards,
+    <br>
+    MusCo Blog Team`, // Use the message content in the HTML field with formatting
   attachments: [
     {
       filename: "MusCo.png",
